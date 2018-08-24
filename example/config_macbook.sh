7c80c5cae932245319295d47eb64e38b08a2a904
david pan <david.gl.pan@gmail.com>
2018-08-24 22:44:31 +0800

    clean repo to adopt autoconf + automake for non-recursive approach

--- example/config_macbook.sh
+++
@@ -1,9 +0,0 @@
-# This bit of hackiness is a workaround for the weird ass "error:
-#   specify a tag with --tag" error.
-export LIBTOOL="glibtool --tag=CXX"
-
-prefix=$HOME/.opt/one_true_makefile
-
-./configure \
-    --prefix=$prefix
-
